9a515938ec54db4012da88727bf6aedaf40901aa
Konstantin Manna <Konstantin.Manna@rewe-digital.com>
2017-04-21 11:32:31 +0200

    asdas

--- src/com/foo/Utilities.groovy
+++ src/com/foo/Utilities.groovy
@@ -1,6 +1,6 @@
 package com.foo
 class Utilities {
   static def mvn(script, args) {
-    script.sh "${script.tool 'Maven 3.3.9'}/bin/mvn -o ${args}"
+    script.sh "echo "Would run maven stuff now; sleep 4"
   }
 }
